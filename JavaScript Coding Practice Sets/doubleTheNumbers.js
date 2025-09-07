572a1809ae0f7629fc5662cf607d75b39b337bd1
Manish Yadav <manishyaduvanshi8294@gmail.com>
2025-09-07 21:35:36 +0530

    Modify the solution, optimize the doubleTheNumbers remove switch case

--- JavaScript Coding Practice Sets/doubleTheNumbers.js
+++ JavaScript Coding Practice Sets/doubleTheNumbers.js
@@ -5,20 +5,10 @@ const arr  = [1, 'David', 10, {'points': 97}, 25, 'alphabet', true];
 
 
 let finalArray = arr.map(currentEl => {
-    switch (typeof currentEl) {
-        case 'number':
-            return currentEl * 2;
-        case 'string':
-            return 'string';
-        case 'boolean':
-            return 'boolean';
-        case 'object':
-            return 'object'; 
-        case 'boolean':
-            return 'boolean'; 
-        default:
-            return currentEl;
+    if (typeof currentEl === "number") {
+        return currentEl * currentEl; 
     }
+    return typeof currentEl; 
 })
 
 
